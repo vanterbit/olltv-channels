4cee46a98310ed2da8c525c588709da7563e4f2a
Vanterbit <protchenko.88@gmail.com>
2016-10-08 15:16:08 +0300

    Подключен phpQuery

---
+++ index.php
@@ -0,0 +1,12 @@
+﻿<?php
+require 'phpQuery.php';
+
+$siteUrl = 'http://oll.tv/tv-channels';
+
+$getSite = file_get_contents($siteUrl);
+
+$doc = phpQuery::newDocument($getSite);
+
+//$tbl = $doc->find('.cont-channels-list');
+$tbl = $doc->find('.cont-channels-list tr:eq(0)');
+echo $tbl;
