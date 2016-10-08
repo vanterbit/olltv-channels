ee0875e504b8c38bf5dced1cb00a5a4c4c47d15a
Vanterbit <protchenko.88@gmail.com>
2016-10-08 19:29:23 +0300

    Вывод каналов в зависимости от тарифа работает

--- index.php
+++ index.php
@@ -7,6 +7,23 @@ $getSite = file_get_contents($siteUrl);
 
 $doc = phpQuery::newDocument($getSite);
 
-//$tbl = $doc->find('.cont-channels-list');
-$tbl = $doc->find('.cont-channels-list tr:eq(0)');
-echo $tbl;
+
+foreach ($doc->find(".cont-channels-list tr") as $article) {
+    $article = pq($article);
+    
+    //Определяем какой тариф выводить - это реализуем позже
+    //2-старт, 3-оптимал, 4-премиальный
+    $tarif = 2;
+    
+    $findTarif = $article->find("td:eq($tarif)")->text();
+    
+    if(!preg_match("/-/i", $findTarif)){
+        $img = $article->find('td img');
+        $nameCannal = $article->find('td:eq(1)')->text();
+        
+        echo "<div class=\"olltvchennels\">"
+            . " <div class=\"namechennel\"><p> {$nameCannal} </p></div> "
+            . "  {$img}  "
+        . " </div>";
+    }
+}
