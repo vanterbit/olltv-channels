d2b2da427dfcbc1b4d7d14f436b49c821774d644
Vanterbit <protchenko.88@gmail.com>
2016-10-10 00:56:51 +0300

    Сохранение изображений реализовано, нооказалось что URL одного и того лого может быть разный, потому нужно реализоваить метод для удаления изображений с папки, а значит стоит подумать.ю возможно для каждого тарифа будет своя папка с изображениями

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 ﻿<?php
 require 'phpQuery.php';
+require 'saveImgOlltvClass.php';
 
 $siteUrl = 'http://oll.tv/tv-channels';
 
@@ -7,6 +8,10 @@ $getSite = file_get_contents($siteUrl);
 
 $doc = phpQuery::newDocument($getSite);
 
+$imgUrl = new saveImgOlltv;
+
+
+static $erro = 1;/////////////////////////////////////////
 
 foreach ($doc->find(".cont-channels-list tr") as $article) {
     $article = pq($article);
@@ -17,8 +22,14 @@ foreach ($doc->find(".cont-channels-list tr") as $article) {
     
     $findTarif = $article->find("td:eq($tarif)")->text();
     
-    if(!preg_match("/-/i", $findTarif)){
+    $imgAttr = $article->find('td img')->attr('src');//получаем URL картинки что бы потом передать для скачивания
+    $urlPattern = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
+    $resultUrl = preg_match($urlPattern, $imgAttr);// тут проверяем если нет Url значит нет изображения и итерация дальше не нужна
+    
+    if(!preg_match("/-/i", $findTarif) and $resultUrl){
+        echo $erro++;////////////////////////////////////////для дебагинга - смотрим номер итерации
         $img = $article->find('td img');
+        $imgUrl->saveFile($imgAttr);// в метод передаем Url изображения по которому получаем его
         $nameCannal = $article->find('td:eq(1)')->text();
         
         echo "<div class=\"olltvchennels\">"
---
+++ saveImgOlltvClass.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Переименовует и сoхраняет файлы по URL
+ * 
+ * @author Prot 
+ */
+class saveImgOlltv {
+
+    /**
+     * Переименовуем полученный файл
+     * 
+     * Поскольку файл который получаем имеет вид URL с слешами, то переименовуем
+     * 
+     * @param string $x  передаем url/имя файла
+     *
+     * @return string   -  возвращаем название файла
+     * 
+     * @author Prot
+     */
+    public function getFileNameImg($x) {
+
+        $path_parts = pathinfo($x);
+        $getExtensionImg = $path_parts['extension']; //получаем расширение файла
+        //
+        //меняем символы http://  / .
+        $patterns = array();
+        $patterns[0] = '/http\:\/\//';
+        $patterns[1] = '/\//';
+        $patterns[2] = '/\./';
+
+        $replacements = array();
+        $replacements[2] = '';
+        $replacements[1] = '-';
+        $replacements[0] = '_';
+
+        $getFileName = preg_replace($patterns, $replacements, $x);
+        $getFullName = $getFileName . "." . $getExtensionImg;
+
+        return $getFullName;
+    }
+
+    /**
+     * Сoхраняет файлы по URL
+     * 
+     * Папку куда нужно сохранять файлы не создается автоматом
+     *
+     *
+     * @param string $url Первый параметр url файла
+     * @param string $path Второй параметр Путь куда сохраняем
+     * 
+     * @author Prot 
+     */
+    public function saveFile($url, $path = "./images/") {
+        $pathNow = $path . $this->getFileNameImg($url);
+        file_put_contents($pathNow, file_get_contents($url));
+    }
+
+}
